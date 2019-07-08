2497d90ce4418b3333ac385ee0c78c032f9c5926
brian vilchez <brianvilchez@brians-MacBook-Pro.local>
2019-07-08 15:07:05 -0600

    finished project

---
+++ NOCList/NOCList/AgentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AgentTableViewCell.swift
+//  NOCList
+//
+//  Created by brian vilchez on 7/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AgentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
