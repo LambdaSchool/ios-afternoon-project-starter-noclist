0e4303dfa8b502a80b2b49e8bedec484b317e905
Clayton Bonaguidi <claytonbonaguidi@gmail.com>
2019-07-10 22:22:53 -0700

    Updating Table View Controller

--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -8,23 +8,35 @@
 
 import UIKit
 
+
 class NOCListTableViewController: UITableViewController
 {
+    
     // MARK: - Properties
+    private var agents: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] = []
     
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
+        
+        title = "Agents"
         super.viewDidLoad()
-        print("Hi Victor")
+        
+        loadNOCList()
+    }
+    
+    
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return agents.count
+        
         
     }
     
     // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
@@ -39,4 +51,24 @@ class NOCListTableViewController: UITableViewController
     }
     
     // MARK: - Private
+    
+    
+    private func loadNOCList()
+    {
+        let agent1 = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: true)
+        let agent2 = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: false)
+        let agent3 = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+        let agent4 = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+        let agent5 = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+        let agent6 = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+        let agent7 = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+        let agent8 = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+        let agent9 = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+        let agent10 = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+        let agent11 = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
+        
+        agents.append(contentsOf: [agent1, agent2, agent3, agent4, agent5, agent6, agent7, agent8, agent9, agent10, agent11])
+        
+    }
+    
 }
