f6c927199f69038daba9f37f1807216defc514fc
John Kouris <jdkouris@gmail.com>
2019-07-10 18:04:57 -0700

    Completed MVP requirements. Ready for review

--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -12,29 +12,87 @@ class NOCListTableViewController: UITableViewController
 {
     // MARK: - Properties
     
+    var agents = [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]()
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Agents"
+        loadNOCList()
     }
     
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return agents.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "agentCell", for: indexPath)
+        
+        let singleAgent = agents[indexPath.row]
+        
+        cell.textLabel?.text = singleAgent.coverName
+        cell.detailTextLabel?.text = singleAgent.realName
+        
+        if singleAgent.compromised {
+            cell.backgroundColor = UIColor(hue: 0, saturation: 0.4, brightness: 0.9, alpha: 1.0)
+        } else {
+            cell.backgroundColor = .white
+        }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "\(compromisedCount()) agents compromised"
     }
     
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        let selectedIndexPath = tableView.indexPathForSelectedRow!
+        let destinationVC = segue.destination as! AgentDetailViewController
+        destinationVC.agent = agents[selectedIndexPath.row]
+        
     }
     
     // MARK: - Private
+    
+    private func loadNOCList() {
+        
+        let ethan = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: true)
+        let jim = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: false)
+        let claire = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+        let eugene = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+        let franz = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+        let luther = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+        let sarah = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+        let max = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+        let hannah = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+        let jack = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+        let frank = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
+        
+        agents.append(contentsOf: [ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank])
+        
+    }
+    
+    private func compromisedCount() -> String {
+        
+        var totalCompromised = 0
+        
+        for singleAgent in agents {
+            if singleAgent.compromised {
+                totalCompromised += 1
+            }
+        }
+        
+        return "\(totalCompromised)"
+        
+    }
+    
+    
 }
