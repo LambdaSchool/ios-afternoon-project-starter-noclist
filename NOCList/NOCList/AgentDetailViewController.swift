9ae452a6598f70270c2121c9f79af8a57b2a79a9
FabiolaSaga <saga.fabiola@gmail.com>
2019-07-12 21:05:35 -0500

    Changed return statement on compromisedCount func

--- NOCList/NOCList/AgentDetailViewController.swift
+++ NOCList/NOCList/AgentDetailViewController.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class AgentDetailViewController: UIViewController {
+    
+    @IBOutlet weak var coverNameLabel: UILabel!
+    @IBOutlet weak var agentRealNameLabel: UILabel!
+    @IBOutlet weak var agentAccessLevelLabel: UILabel!
+    
+    var agent: (coverName: String, realName: String, accessLevel: Int, compromised: Bool)!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        coverNameLabel.text = agent.coverName
+        agentRealNameLabel.text = agent.realName
+        agentAccessLevelLabel.text = String(agent.accessLevel)
+        
+        if agent.compromised {
+            view.backgroundColor = UIColor(hue: 0, saturation: 0.4, brightness: 0.9, alpha: 1.0)
+        } else {
+            view.backgroundColor = .white
+        }
     }
     
 
