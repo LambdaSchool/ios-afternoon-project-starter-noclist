d4a76015c2a7298cb63395aca5362e031129af4e
swiftalicious <amazinggrace1@protonmail.com>
2019-07-18 16:09:24 -0700

    Added print statement

--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -16,6 +16,7 @@ class NOCListTableViewController: UITableViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello")
     }
     
     // MARK: - Table view data source
