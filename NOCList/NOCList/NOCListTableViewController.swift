01058047dbb058339a5c993dd821ee20a423a04e
ryandutson <mr.dutson@gmail.com>
2019-08-05 18:24:42 -0600

    Almost complete version of the day's project.

--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -8,22 +8,42 @@
 
 import UIKit
 
-class NOCListTableViewController: UITableViewController
-{
+class NOCListTableViewController: UITableViewController {
+    
+    private var agents: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] = []
+    
+    
     // MARK: - View Lifecycle
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadNOCList()
     }
     
     // MARK: - Table view data source
     
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+       return agents.count
+        
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return agents.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NOCListCell", for: indexPath)
+    
+        let anAgent = agents[indexPath.row]
+        
+        cell.textLabel?.text = anAgent.coverName
+        cell.detailTextLabel?.text = anAgent.realName
+        if anAgent.compromised == true {
+            cell.backgroundColor = UIColor(hue: 0, saturation: 0.4, brightness: 0.9, alpha: 1.0)
+        } else {
+            cell.backgroundColor = .white
+        }
         
         return cell
     }
@@ -35,4 +55,31 @@ class NOCListTableViewController: UITableViewController
     }
     
     // MARK: - Private
+    
+    private func loadNOCList() {
+        let agent1 = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: true)
+        let agent2 = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: false)
+        let agent3 = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+        let agent4 = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+        let agent5 = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+        let agent6 = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+        let agent7 = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+        let agent8 = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+        let agent9 = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+        let agent10 = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+        let agent11 = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
+   
+        agents.append(contentsOf: [agent1, agent2, agent3, agent4, agent5 ,agent6, agent7, agent8, agent9, agent10, agent11])
+        
+    }
+    
+    private func compromisedCount() -> String {
+        var totalCount = 0
+        for _ in agents {
+            totalCount += agents.count
+        }
+        
+        return "\(compromisedCount()) agents compromised"
+    }
+
 }
