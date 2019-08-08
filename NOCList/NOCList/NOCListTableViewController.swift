9473c7344eb4d7c1da7a3514e1a02383e27a32b1
Ben Gohlke <jcgohlke@me.com>
2019-08-08 13:57:19 -0400

    Updated row count

--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -19,7 +19,7 @@ class NOCListTableViewController: UITableViewController
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
